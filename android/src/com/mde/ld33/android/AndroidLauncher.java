ae0231ef4a4abb0b6ee72e811ada44c9b378cc28
MDeiml <matthias@deiml.net>
2015-08-21 20:15:37 +0200

    Created libgdx project

---
+++ android/src/com/mde/ld33/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mde.ld33.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mde.ld33.LD33;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LD33(), config);
+	}
+}
---
+++ desktop/src/com/mde/ld33/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mde.ld33.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mde.ld33.LD33;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD33(), config);
+	}
+}
